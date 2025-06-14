17451a1b4b7d2ae18dede7eebb87f1fa759d68bd
ibrahimssquare768 <ibrahim@ssquare.pk>
2025-06-14 12:55:49 +0500

    created users table

--- src/migrations/20250613112019_create_table_users.js
+++ src/migrations/20250613112019_create_table_users.js
@@ -3,7 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+
+    return knex.schema.createTable('users' , (table) =>{
+        table.increments('id').primary();
+        table.string('full_name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        table.timestamps(true , true);
+    })
 };
 
 /**
@@ -11,5 +18,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
